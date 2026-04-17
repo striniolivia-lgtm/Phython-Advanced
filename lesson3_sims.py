a82069084ec2784a264b337a04949e8bcbcdde81
olivia <striniolivia@gmail.com>
2026-04-17 20:01:01 +0200

    initial sims simulation

--- lesson3_sims.py
+++ lesson3_sims.py
@@ -1,4 +1,6 @@
 import random
+
+
 class Human:
     def __init__(self, name="Human", job=None, home=None, car=None):
         self.name = name
@@ -10,13 +12,64 @@ class Human:
         self.home = home
 
     def get_home(self):
-        pass
+        self.home = House()
 
     def get_car(self):
-        pass
+        self.car = Car(brands_of_car)
 
     def get_job(self):
         pass
+
+    def eat(self):
+        if self.home.food <= 0:
+            pass  # add shopping method
+        else:
+            if self.satiety >= 100:
+                self.satiety = 100
+                return
+            self.satiety += 5
+            self.home.food -= 5
+
+    def work(self):
+        if self.car.drive():
+            pass
+        else:
+            if self.car.fuel < 20:
+                pass  # shopping
+            else:
+                pass  # to repair
+
+        self.money += self.job.salary
+        self.gladness += self.job.gladness_less
+        self.satiety -= 4
+
+    def shopping(self, manage):
+        if self.car.drive():
+            pass
+        else:
+            if self.car.fuel < 20:
+                manage = "fuel"
+            else:
+                pass  # repair car
+
+        if manage == "fuel":
+            print("I bought fuel!")
+            self.money -= 100
+            self.car.fuel += 100
+        elif manage == "food":
+            print("I bought food!")
+            self.money -= 50
+            self.home.food += 50
+        elif manage == "kebap":
+            print("Banger!")
+            self.gladness += 10
+            self.satiety += 2
+            self.money -= 15
+
+    def chill(self):
+        self.gladness += 10
+        self.home.mess += 5
+
 
 
 class Car:
@@ -24,12 +77,42 @@ class Car:
         self.brand = random.choice(list(brand_list))
         self.fuel = brand_list[self.brand]["fuel"]
         self.strength = brand_list[self.brand]["strength"]
-        self.consumtion = brand_list[self.brand]["consumtion"]
+        self.consumption = brand_list[self.brand]["consumption"]
+
+    def drive(self):
+        if self.strength > 0 and self.fuel >= self.consumption:
+            self.fuel -= self.consumption
+            self.strength -= 1
+            return True
+        else:
+            print("The car cannot move!")
+            return False
+
+
+class House:
+    def __init__(self):
+        self.mess = 0
+        self.food = 0
+
+
+class Job:
+    def __init__(self, job_list):
+        self.job = random.choice(list(job_list))
+        self.salary = job_list[self.job]["salary"]
+        self.gladness_less = job_list[self.job]["gladness_less"]
+
+
+job_list = {
+    "IT-Developer": {"salary": 50, "gladness_less": 10},
+    "Mercenary": {"salary": 100, "gladness_less": 3},
+    "Striper": {"salary": 150, "gladness_less": 30},
+    "Furry": {"salary": 20, "gladness_less": 8}
+}
 
 
 brands_of_car = {
-    "BMW": {"fuel": 100, "strength": 100, "consumtion": 6},
-    "Farrari": {"fuel": 200, "strength": 500, "consumtion": 10},
-    "Lamborgihni": {"fuel": 200, "strength": 500, "consumtion": 9},
-    "Bugati": {"fuel": 230, "strength": 1000, "consumtion": 10}
+    "BMW": {"fuel": 100, "strength": 100, "consumption": 6},
+    "Farrari": {"fuel": 200, "strength": 500, "consumption": 10},
+    "Lamborgihni": {"fuel": 200, "strength": 500, "consumption": 9},
+    "Bugati": {"fuel": 230, "strength": 1000, "consumption": 10}
 }
